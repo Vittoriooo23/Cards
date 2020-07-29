d5acc1edbd6ef778930961e3a3b8d002d94210aa
Vittoriooo23 <russovictor96@gmail.com>
2020-07-29 14:35:34 -0400

    Creating jar

--- src/Cards/Main.java
+++ src/Cards/Main.java
@@ -5,10 +5,5 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[]args) throws FileNotFoundException {
         Deck deck = new Deck();
-
-        Card card = new Card();
-        card = deck.getRandomCard();
-
-        System.out.println(card);
     }
 }
